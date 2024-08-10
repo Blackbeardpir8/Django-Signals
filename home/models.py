72b891e1c6a23dfca2b0262f0d4ead1b3b4799a1
Blackbeardpir8 <deepakdk15yadav@gmail.com>
2024-08-10 11:41:21 +0530

    post save signal

--- home/models.py
+++ home/models.py
@@ -1,7 +1,16 @@
 from django.db import models
-
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 # Create your models here.
 class Student(models.Model):
     student_name = models.CharField(max_length=100)
     student_gender = models.CharField(max_length=10,choices=[('male', 'Male'),('female', 'Female')])
- 
+    student_id = models.CharField(max_length=10, null=True,blank=True)
+
+@receiver(post_save, sender = Student)
+def save_student(sender, instance,created, **kwargs):
+    print(sender, instance)
+    if created:
+        instance.student_id = f"STU-000{instance.id}"
+        instance.save()
+        print("Student object is Created")
