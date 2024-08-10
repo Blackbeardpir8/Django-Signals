bc390e2b737badcba16d734ec2a7cff51ea6a159
Blackbeardpir8 <deepakdk15yadav@gmail.com>
2024-08-10 10:41:16 +0530

    basic app iss k baad signal add krenge

--- home/models.py
+++ home/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class Student(models.Model):
+    student_name = models.CharField(max_length=100)
+    student_gender = models.CharField(max_length=10,choices=[('male', 'Male'),('female', 'Female')])
+ 
