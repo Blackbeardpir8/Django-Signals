3160ef549c482a195c001859af6566358ac0bffd
Blackbeardpir8 <deepakdk15yadav@gmail.com>
2024-08-10 11:45:02 +0530

    pre delete signals

--- home/models.py
+++ home/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from django.db.models.signals import pre_save
+from django.db.models.signals import pre_delete
 from django.dispatch import receiver
 # Create your models here.
 class Student(models.Model):
@@ -7,10 +7,6 @@ class Student(models.Model):
     student_gender = models.CharField(max_length=10,choices=[('male', 'Male'),('female', 'Female')])
     student_id = models.CharField(max_length=10, null=True,blank=True)
 
-@receiver(pre_save, sender = Student)
-def save_student(sender, instance,created, **kwargs):
-    print(sender, instance)
-    if created:
-        instance.student_id = f"STU-000{instance.id}"
-        instance.save()
-        print("Student object is Created")
+@receiver(pre_delete, sender = Student)
+def save_student(sender, instance, **kwargs):
+    print("OBJ Getting Deleted")
